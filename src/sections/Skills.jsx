a62e5fdea5b35580953e7cd7f90e4a42755689a7
aayanofficial5 <aayanofficial5@gmail.com>
2025-05-18 00:28:47 +0530

    refactor: update Skills section layout and improve styling for better presentation

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,27 +1,27 @@
 import { motion } from 'framer-motion';
 import LanguageIcon from '../components/LanguageIcon';
-import { languages } from '../data';
+import { skills } from '../data';
 import SkillCard from '../components/SkillCard';
 const Skills = () => {
   return (
     <section
   id="skills"
-  className="flex flex-col justify-center bg-background dark:bg-background h-[70vh] w-full border-t dark:border-secondary/50 overflow-visible"
+  className="flex flex-col items-center bg-background dark:bg-background py-10 px-10 md:px-20 w-full border-t dark:border-secondary/50 min-h-screen"
 >
   <motion.div
     initial={{ opacity: 0, y: 40 }}
     whileInView={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.6 }}
     viewport={{ once: true }}
-    className="max-w-6xl mx-auto flex flex-col gap-5 overflow-visible"
+    className="max-w-6xl flex flex-col items-center"
   >
     <h2 className="text-3xl md:text-4xl font-bold text-center text-primary dark:text-primary mb-6">
       Skills
     </h2>
 
-    <div className="flex gap-4 flex-wrap max-w-[90vw] relative z-0 overflow-visible">
+    <div className="grid grid-cols-4 md:grid-cols-8 space-y-12 space-x-12 max-w-[90vw] relative pl-10 pt-8">
 
-      {languages.map((language, index) => (
+      {skills.map((language, index) => (
         <SkillCard language={language} key={index} />
       ))}
     </div>
